1432da75bd17cd428940b81aef6bf3e273ea9fbc
noureddine haddad <noureddinehaddad17@gmail.com>
2022-01-26 21:41:39 +0100

    last

--- src/App.js
+++ src/App.js
@@ -1,177 +1,181 @@
-import { useEffect, useState } from 'react'
-import './styles/main.scss'
-import Navigation from './components/Navigation';
-import ImageLinkForm from './components/ImageLinkForm';
-import Particles from 'react-particles-js';
-import Clarifai from 'clarifai'
-import FaceRecognition from './components/FaceRecognition';
-import Register from './components/Register';
-import Signin from './components/Signin';
-import NotFoundImage from './images/NotFound.png'
-
-
+import { useEffect, useState } from "react";
+import "./styles/main.scss";
+import Navigation from "./components/Navigation";
+import ImageLinkForm from "./components/ImageLinkForm";
+import Particles from "react-particles-js";
+import Clarifai from "clarifai";
+import FaceRecognition from "./components/FaceRecognition";
+import Register from "./components/Register";
+import Signin from "./components/Signin";
+import NotFoundImage from "./images/NotFound.png";
 
 const particlesOptions = {
-  particles: {
-    number:{
-      value:30,
-      density: {
-        enable: true,
-        value_area: 800
-      }
-    },
-    line_linked:{
-      enable:true,
-      distance:150,
-      color:"#000000",
-      opacity:0.5
-  }
-  }
-}
-
+   particles: {
+      number: {
+         value: 30,
+         density: {
+            enable: true,
+            value_area: 800,
+         },
+      },
+      line_linked: {
+         enable: true,
+         distance: 150,
+         color: "#000000",
+         opacity: 0.5,
+      },
+   },
+};
 
 function App() {
+   const [inputValue, setInputValue] = useState("");
+   const [imageUrl, setImageUrl] = useState(NotFoundImage);
+   const [boundingBox, setBoundingBox] = useState("");
+   const [isSignin, setIsSignin] = useState(false);
+   const [route, setRoute] = useState("signin");
+   const [user, setUser] = useState({
+      id: "",
+      name: "default",
+      email: "",
+      entries: "0",
+      joined: "",
+   });
 
-  const [inputValue, setInputValue] = useState('')
-  const [imageUrl, setImageUrl] = useState(NotFoundImage)
-  const [boundingBox, setBoundingBox] = useState('')
-  const [isSignin, setIsSignin] = useState(false)
-  const [route, setRoute] = useState('signin')
-  const [user, setUser] = useState({
-    id: '',
-    name: 'default',
-    email: '',
-    entries: '0',
-    joined: ''
-  })
-  
-  useEffect(() => {
-    resetState()
-  }, [])
+   const BASE_URL = "https://desolate-coast-02851.herokuapp.com";
 
-  function resetState(){
-    setInputValue("")
-    setImageUrl(NotFoundImage)
-    setBoundingBox('')
-  }
+   useEffect(() => {
+      resetState();
+   }, []);
 
+   function resetState() {
+      setInputValue("");
+      setImageUrl(NotFoundImage);
+      setBoundingBox("");
+   }
 
-  const onInputChange = (event) => {
-    console.log(event.target.value);
-    setInputValue(event.target.value)
-  }
+   const onInputChange = (event) => {
+      console.log(event.target.value);
+      setInputValue(event.target.value);
+   };
 
-  const loadUser = (data) => {
-    setUser({
-      id: data.id,
-      name: data.name,
-      email: data.email,
-      entries: data.entries,
-      joined: data.joined
-    })
-  }
+   const loadUser = (data) => {
+      setUser({
+         id: data.id,
+         name: data.name,
+         email: data.email,
+         entries: data.entries,
+         joined: data.joined,
+      });
+   };
 
+   const onSubmit = () => {
+      document.getElementById("face-image-holder").classList.add("hide");
 
-  const onSubmit = () => {
-    document.getElementById('face-image-holder').classList.add('hide')
+      setImageUrl(inputValue);
 
-    setImageUrl(inputValue)
-
-    fetch("http://localhost:4000/imageurl", {
-      method: "POST",
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({
-        input: inputValue
+      fetch(BASE_URL + "/imageurl", {
+         method: "POST",
+         headers: { "Content-Type": "application/json" },
+         body: JSON.stringify({
+            input: inputValue,
+         }),
       })
-    })
-    .then(res => res.json())
-    .then(result => {
-      console.log(result.outputs[0].data.regions[0].region_info.bounding_box)
-      setBoundingBox(result.outputs[0].data.regions[0].region_info.bounding_box)
-      document.getElementById('box').classList.remove('hide')
-      document.getElementById('face-image-holder').classList.remove('hide')
+         .then((res) => res.json())
+         .then((result) => {
+            console.log(
+               result.outputs[0].data.regions[0].region_info.bounding_box
+            );
+            setBoundingBox(
+               result.outputs[0].data.regions[0].region_info.bounding_box
+            );
+            document.getElementById("box").classList.remove("hide");
+            document
+               .getElementById("face-image-holder")
+               .classList.remove("hide");
 
-        fetch("http://localhost:4000/image", {
-          method: "PUT",
-          headers: {'Content-Type': 'application/json'},
-          body: JSON.stringify({
-            id: user.id
-          })
-        })
-        .then(response => response.json())
-        .then(result => {
-          setUser({
-            ...user,
-            entries: result.entries
-          })
-          console.log(result)
-        })
-        .catch(err => {
-          console.log(err);
-        })
+            fetch(BASE_URL + "/image", {
+               method: "PUT",
+               headers: { "Content-Type": "application/json" },
+               body: JSON.stringify({
+                  id: user.id,
+               }),
+            })
+               .then((response) => response.json())
+               .then((result) => {
+                  setUser({
+                     ...user,
+                     entries: result,
+                  });
+                  console.log(result);
+               })
+               .catch((err) => {
+                  console.log(err);
+               });
+         })
+         .catch((err) => {
+            document.getElementById("image").setAttribute("src", NotFoundImage);
+            document.getElementById("box").classList.add("hide");
+            document
+               .getElementById("face-image-holder")
+               .classList.remove("hide");
+         });
+   };
 
-    })
-    .catch(err => {
-      document.getElementById('image').setAttribute('src', NotFoundImage)
-      document.getElementById('box').classList.add('hide')
-      document.getElementById('face-image-holder').classList.remove('hide')
-    });
-  }
+   const changeRoute = (route) => {
+      if (route === "home") {
+         setIsSignin(true);
+         resetState();
+      } else {
+         setIsSignin(false);
+      }
 
-  const changeRoute = (route) => {
-    if(route === 'home'){
-      setIsSignin(true)
-      resetState()
-    }else{
-      setIsSignin(false)
-    }
+      console.log(route);
+      setRoute(route);
+   };
 
-    console.log(route);
-    setRoute(route)
-  }
+   return (
+      <div className="App">
+         <Particles className="particles" params={particlesOptions} />
+         <Navigation isSignin={isSignin} changeRoute={changeRoute} />
+         <div className="main">
+            {route === "home" ? (
+               <>
+                  <div className=" text-center">
+                     <p
+                        style={{
+                           textJustify: "center",
+                        }}
+                     >
+                        This Magin Brain will detect faces in your pictures. Git
+                        it a try.
+                     </p>
 
+                     <br />
+                     <br />
+                     <h2>Welcome {user.name}</h2>
+                     <h4>entries : {user.entries}</h4>
+                  </div>
 
-  return (
-    <div className="App">
-      <Particles 
-        className="particles"
-        params={particlesOptions}
-        
-      />
-      <Navigation isSignin={isSignin} changeRoute={changeRoute} />
-      <div className="main" >
-        {
-          route === 'home' 
-          ? <>
-            <div className=" text-center">
-              <p style={{
-                  textJustify:'center'
-                }}>
-                This Magin Brain will detect faces in your pictures. Git it a try.
-              </p>                
+                  <div className=" p-4 row justify-content-center">
+                     <ImageLinkForm
+                        onInputChange={onInputChange}
+                        onSubmit={onSubmit}
+                     />
+                  </div>
 
-              <br/><br/>
-              <h2>Welcome {user.name}</h2>
-              <h4>entries : {user.entries}</h4>
-            </div>
-
-            <div className=" p-4 row justify-content-center" >      
-              <ImageLinkForm onInputChange={onInputChange} onSubmit={onSubmit} />
-            </div>
-
-            <FaceRecognition imageUrl={imageUrl} boundingBox={boundingBox} />
-          </>
-          : route === 'signin'
-            ? <Signin loadUser={loadUser} changeRoute={changeRoute}  />
-            : <Register loadUser={loadUser} changeRoute={changeRoute}  />
-          
-
-
-        }
-
+                  <FaceRecognition
+                     imageUrl={imageUrl}
+                     boundingBox={boundingBox}
+                  />
+               </>
+            ) : route === "signin" ? (
+               <Signin loadUser={loadUser} changeRoute={changeRoute} />
+            ) : (
+               <Register loadUser={loadUser} changeRoute={changeRoute} />
+            )}
+         </div>
       </div>
-    </div>
-  );
+   );
 }
 
 export default App;
--- src/components/FaceRecognition.jsx
+++ src/components/FaceRecognition.jsx
@@ -1,31 +1,42 @@
-import React from 'react'
+import React from "react";
 
 function FaceRecognition(props) {
-    return (
-        <div style={{
-            display:'flex', justifyContent:'center',marginTop:'10px'
-            }}>
-            <div id='face-image-holder' style={{
-                position:'relative'
-            }}>
-                <img id="image" src={props.imageUrl} alt="" style={{width:'500px', height:'100%'}} 
-                    
-                />
-                <div id="box" className=' hide' style={{
-                    border:'2px solid blue',
-                    position:'absolute',
-                    top:` ${props.boundingBox.top_row * 100}%`,
-                    left:` ${props.boundingBox.left_col * 100}%`,
-                    right:` ${(1 - props.boundingBox.right_col) * 100}%`,
-                    bottom:` ${(1 - props.boundingBox.bottom_row) * 100}%`,
-                    paddingTop:'30px'
-                }}>
-            </div>
-            
-                
-            </div>
-        </div>
-    )
+   return (
+      <div
+         style={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "10px",
+         }}
+      >
+         <div
+            id="face-image-holder"
+            style={{
+               position: "relative",
+            }}
+         >
+            <img
+               id="image"
+               src={props.imageUrl}
+               alt=""
+               style={{ width: "500px", height: "100%" }}
+            />
+            <div
+               id="box"
+               className=" hide"
+               style={{
+                  border: "2px solid blue",
+                  position: "absolute",
+                  top: ` ${props.boundingBox.top_row * 100}%`,
+                  left: ` ${props.boundingBox.left_col * 100}%`,
+                  right: ` ${(1 - props.boundingBox.right_col) * 100}%`,
+                  bottom: ` ${(1 - props.boundingBox.bottom_row) * 100}%`,
+                  paddingTop: "30px",
+               }}
+            ></div>
+         </div>
+      </div>
+   );
 }
 
-export default FaceRecognition
+export default FaceRecognition;
